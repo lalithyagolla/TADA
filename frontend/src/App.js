c7b27d2ad74fd137efe9d8153e3ac3f73cc4fc1a
Lalithya Golla <108351773+lalithyagolla@users.noreply.github.com>
2025-06-14 09:10:53 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   const [expenses, setExpenses] = useState([]);
 
   useEffect(() => {
-    axios.get('https://tada-backend-zkpy.onrender.com')
+    axios.get('http://localhost:5000/api/expenses')
       .then(res => setExpenses(res.data));
   }, []);
 
